e5dcaa47d4502398bb744aa7e403a087f4ab87b1
Adam Knowles <acknowles524@gmail.com>
2019-09-22 22:07:46 -0500

    delete working for detail views

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -9,12 +9,16 @@ app_name = "libraryapp"
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^books$', book_list, name='books'),
+    url(r'^libraries$', library_list, name='libraries'),
+    url(r'^library/form$', library_form, name='library_form'),
     url(r'^librarians$', list_librarians, name='librarians'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^logout/$', logout_user, name='logout'),
     url(r'^book/form$', book_form, name='book_form'),
     url(r'^books/(?P<book_id>[0-9]+)/form$', book_edit_form, name='book_edit_form'),
     path('books/<int:book_id>/', book_details, name='book'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
+    
     
     
 ]
